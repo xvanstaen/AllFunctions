e2e0181fa9aa5f03e5ad6d7eb0fabe9810acf5a1
xvanstaen <xvanstaen@gmail.com>
2023-08-08 21:37:51 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -69,7 +69,7 @@ inData=new classAccessFile;
             if (data[i].title==="configServer" && data[i].test_prod===test_prod){
                 this.configServer = data[i];
 
-                this.configServer.baseUrl='http://localhost:8080';
+               // this.configServer.baseUrl='http://localhost:8080';
             
             } else if (data[i].title==="configPhoto" && data[i].test_prod===test_prod){
                 this.XMVConfig = data[i];
